f600ec65112a126751ab6e678179f48879466deb
n-bor1sov <n.bor1sov@yandex.ru>
2025-03-19 17:31:07 +0300

    lab4

--- src/renderer/rasterizer/rasterizer.h
+++ src/renderer/rasterizer/rasterizer.h
@@ -96,9 +96,65 @@ namespace cg::renderer
 	template<typename VB, typename RT>
 	inline void rasterizer<VB, RT>::draw(size_t num_vertexes, size_t vertex_offset)
 	{
-		// TODO Lab: 1.04 Implement `cg::world::camera` class
-		// TODO Lab: 1.05 Add `Rasterization` and `Pixel shader` stages to `draw` method of `cg::renderer::rasterizer`
-		// TODO Lab: 1.06 Add `Depth test` stage to `draw` method of `cg::renderer::rasterizer`
+		size_t vertex_id = vertex_offset;
+		while (vertex_id < vertex_offset + num_vertexes) {
+			std::vector<VB> vertices(3);
+			vertices[0] = vertex_buffer->item(index_buffer->item(vertex_id++));
+			vertices[1] = vertex_buffer->item(index_buffer->item(vertex_id++));
+			vertices[2] = vertex_buffer->item(index_buffer->item(vertex_id++));
+
+			for (auto& vertex: vertices) {
+				float4 coords{vertex.x, vertex.y, vertex.z, 1.f};
+				auto processed_vertex = vertex_shader(coords, vertex);
+
+				vertex.x = processed_vertex.first.x / processed_vertex.first.w;
+				vertex.y = processed_vertex.first.y / processed_vertex.first.w;
+				vertex.z = processed_vertex.first.z / processed_vertex.first.w;
+
+				vertex.x = (vertex.x + 1.f) * width / 2.f;
+				vertex.y = (-vertex.y + 1.f) * height / 2.f;
+			}
+			// float2 vertex_a = float2{vertices[0].x, vertices[0].y};
+			// float2 vertex_b = float2{vertices[1].x, vertices[1].y};
+			// float2 vertex_c = float2{vertices[2].x, vertices[2].y};
+
+			// float2 min_vertex = min(vertex_a, min(vertex_b, vertex_c));
+			// float2 bounding_box_begin = round(clamp(
+			// 		min_vertex, float2{0.f, 0.f},
+			// 		float2{static_cast<float>(width - 1), static_cast<float>(height - 1)}));
+
+			// float2 max_vertex = max(vertex_a, max(vertex_b, vertex_c));
+			// float2 bounding_box_end = round(clamp(
+			// 		max_vertex, float2{0.f, 0.f},
+			// 		float2{static_cast<float>(width - 1), static_cast<float>(height - 1)}));
+
+			// float edge = edge_function(vertex_a, vertex_b, vertex_c);
+
+			// for (float x = bounding_box_begin.x; x <= bounding_box_end.x; x += 1.f) {
+			// 	for (float y = bounding_box_begin.y; y <= bounding_box_end.y; y += 1.f) {
+			// 		float2 point{x, y};
+			// 		float edge0 = edge_function(vertex_a, vertex_b, point);
+			// 		float edge1 = edge_function(vertex_b, vertex_c, point);
+			// 		float edge2 = edge_function(vertex_c, vertex_a, point);
+			// 		if (edge0 >= 0.f && edge1 >= 0.f && edge2 >= 0.f) {
+			// 			size_t u_x = static_cast<float>(x);
+			// 			size_t u_y = static_cast<float>(y);
+
+			// 			float u = edge1 / edge;
+			// 			float v = edge2 / edge;
+			// 			float w = edge0 / edge;
+
+			// 			float z = u * vertices[0].z + v * vertices[1].z + w * vertices[2].z;
+
+			// 			if (depth_test(z, u_x, u_y)) {
+			// 				auto pixel_result = pixel_shader(vertices[0], 0);
+			// 				render_target->item(u_x, u_y) = RT::from_color(pixel_result);
+			// 				depth_buffer->item(u_x, u_y) = z;
+			// 			}
+			// 		}
+			// 	}
+			// }
+		}
 	}
 
 	template<typename VB, typename RT>
