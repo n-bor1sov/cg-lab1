9c6eff7d42f9715fc20e44d294ef392ede784a7f
n-bor1sov <n.bor1sov@yandex.ru>
2025-03-19 15:32:35 +0300

    lab03

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -5,17 +5,46 @@
 
 void cg::renderer::rasterization_renderer::init()
 {
-	// TODO Lab: 1.02 Implement image clearing & saving in `cg::renderer::rasterization_renderer` class
-	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+	rasterizer = std::make_shared<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
+	rasterizer->set_viewport(settings->width, settings->height);
+	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
+		settings->width, settings->height
+	);
+	rasterizer->set_render_target(render_target);
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
+
+	for(size_t i = 0; i < model->get_index_buffers().size(); i++) {
+		auto vertex_buffer_size = model->get_vertex_buffers()[i]->size_bytes();
+		auto index_buffer_size = model->get_index_buffers()[i]->size_bytes();
+
+		auto pure_vertex_buffer_size = model->get_index_buffers()[i]->count()*sizeof(cg::vertex);
+		std::cout << "Vertex buffer size: " << vertex_buffer_size << '\n';
+		std::cout << "Index buffer size: " << index_buffer_size << '\n';
+		std::cout << "Pure vertex buffer size: " << pure_vertex_buffer_size << '\n';
+		std::cout << "Saving: " << pure_vertex_buffer_size - vertex_buffer_size - index_buffer_size << '\n';
+	}
 	// TODO Lab: 1.04 Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	// TODO Lab: 1.06 Add depth buffer in `cg::renderer::rasterization_renderer`
 }
 void cg::renderer::rasterization_renderer::render()
 {
-	// TODO Lab: 1.02 Implement image clearing & saving in `cg::renderer::rasterization_renderer` class
+	auto start = std::chrono::high_resolution_clock::now();
+	rasterizer->clear_render_target({255, 255, 255});
+	auto stop = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float, std::milli> duration = stop - start;
+	std::cout << "CLearing took " << duration.count() << std::endl;
+
 	// TODO Lab: 1.04 Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
 	// TODO Lab: 1.05 Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
-	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+	for (size_t shape_id = 0; shape_id<model->get_index_buffers().size();shape_id++) {
+		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
+		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
+		rasterizer->draw(
+			model->get_index_buffers()[shape_id]->count(), 0
+		);
+	}
+	cg::utils::save_resource(*render_target, settings->result_path);
 }
 
 void cg::renderer::rasterization_renderer::destroy() {}
